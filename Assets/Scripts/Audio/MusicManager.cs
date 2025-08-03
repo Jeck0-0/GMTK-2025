fcaac41dca9eb553da84e4fa7891934aeefa9eb6
Seva <vsevolod.kolomytsev@student.saedubai.comProfile image>
2025-08-03 16:00:05 +0400

    Perfect and seamless music loop

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    [SerializeField] AudioSource introSource;
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioClip introClip;
     [SerializeField] AudioClip musicClip;
     [SerializeField] float volume;
-    private AudioSource musicSource;
 
     private static MusicManager _instance;
 
@@ -41,7 +43,6 @@ public class MusicManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
             _instance = this;
         }
-        musicSource = GetComponent<AudioSource>();
     }
     #endregion
 
@@ -51,10 +52,18 @@ public class MusicManager : MonoBehaviour
     }
     public void StartMusic()
     {
+        double startTime = AudioSettings.dspTime + 0.1f;
+
+        introSource.clip = introClip;
+        introSource.volume = volume;
+        introSource.loop = false;
+        introSource.PlayScheduled(startTime);
+
+        double introEndTime = startTime + introClip.length;
         musicSource.clip = musicClip;
         musicSource.volume = volume;
         musicSource.loop = true;
-        musicSource.Play();
+        musicSource.PlayScheduled(introEndTime);
     }
     public void StopMusic(float fadeDuration = 1f)
     {
@@ -67,8 +76,13 @@ public class MusicManager : MonoBehaviour
         while (musicSource.volume > 0)
         {
             musicSource.volume -= startVolume * Time.deltaTime / duration;
+            if (introSource != null)
+            introSource.volume = musicSource.volume;
             yield return null;
         }
+
         musicSource.Stop();
+        if (introSource != null)
+        introSource.Stop();
     }
 }
